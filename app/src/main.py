28b41211852f92fedf693c700b2d5df3017c03e3
kianwasabi <mehrretischgeschmack@gmail.com>
2023-11-29 15:48:46 +0100

    release testing

--- app/src/main.py
+++ app/src/main.py
@@ -35,7 +35,19 @@ logger = logging.getLogger(__name__)
 
 GET_SPEED_REQUEST_TOPIC = "sampleapp/getSpeed"
 GET_SPEED_RESPONSE_TOPIC = "sampleapp/getSpeed/response"
-DATABROKER_SUBSCRIPTION_TOPIC = "sampleapp/currentSpeed"
+GET_STEER_REQUEST_TOPIC = "sampleapp/getSteer"
+GET_STEER_RESPONSE_TOPIC = "sampleapp/getSteer/response"
+GET_THROT_REQUEST_TOPIC = "sampleapp/getThrottle"
+GET_THROT_RESPONSE_TOPIC = "sampleapp/getThrottle/response"
+GET_BRAKE_REQUEST_TOPIC = "sampleapp/getBrake"
+GET_BRAKE_RESPONSE_TOPIC = "sampleapp/getBrake/response"
+GET_LANE_REQUEST_TOPIC = "sampleapp/getLane"
+GET_LANE_RESPONSE_TOPIC = "sampleapp/getLane/response"
+DATABROKER_SPEED_SUBSCRIPTION_TOPIC = "sampleapp/currentSpeed"
+DATABROKER_STEER_SUBSCRIPTION_TOPIC = "sampleapp/currentSteer"
+DATABROKER_TRHOT_SUBSCRIPTION_TOPIC = "sampleapp/currentThrottle"
+DATABROKER_BRAKE_SUBSCRIPTION_TOPIC = "sampleapp/currentBrake"
+DATABROKER_LANE_SUBSCRIPTION_TOPIC = "sampleapp/currentLane"
 
 
 class SampleApp(VehicleApp):
@@ -64,6 +76,12 @@ class SampleApp(VehicleApp):
         # Vehicle DataBroker is ready.
         # Here you can subscribe for the Vehicle Signals update (e.g. Vehicle Speed).
         await self.Vehicle.Speed.subscribe(self.on_speed_change)
+        await self.Vehicle.Chassis.SteeringWheel.Angle.subscribe(self.on_steer_change)
+        await self.Vehicle.OBD.ThrottlePosition.subscribe(self.on_throttle_change)
+        await self.Vehicle.Chassis.Brake.PedalPosition.subscribe(self.on_brake_change)
+        await self.Vehicle.ADAS.LaneDepartureDetection.IsWarning.subscribe(
+            self.on_lane_change
+        )
 
     async def on_speed_change(self, data: DataPointReply):
         """The on_speed_change callback, this will be executed when receiving a new
@@ -77,8 +95,39 @@ class SampleApp(VehicleApp):
         # Example:
         # - Publishes current speed to MQTT Topic (i.e. DATABROKER_SUBSCRIPTION_TOPIC).
         await self.publish_event(
-            DATABROKER_SUBSCRIPTION_TOPIC,
+            DATABROKER_SPEED_SUBSCRIPTION_TOPIC,
             json.dumps({"speed": vehicle_speed}),
+        )
+
+    async def on_steer_change(self, data: DataPointReply):
+        steering_angle = data.get(self.Vehicle.Chassis.SteeringWheel.Angle).value
+        await self.publish_event(
+            DATABROKER_STEER_SUBSCRIPTION_TOPIC,
+            json.dumps({"steeringAngle": steering_angle}),
+        )
+
+    # 스로틀 위치 변경에 대한 콜백 메서드
+    async def on_throttle_change(self, data: DataPointReply):
+        throttle_position = data.get(self.Vehicle.OBD.ThrottlePosition).value
+        await self.publish_event(
+            DATABROKER_TRHOT_SUBSCRIPTION_TOPIC,
+            json.dumps({"throttlePosition": throttle_position}),
+        )
+
+    # 브레이크 위치 변경에 대한 콜백 메서드
+    async def on_brake_change(self, data: DataPointReply):
+        brake = data.get(self.Vehicle.Chassis.Brake.PedalPosition).value
+        await self.publish_event(
+            DATABROKER_BRAKE_SUBSCRIPTION_TOPIC,
+            json.dumps({"brakePosition": brake}),
+        )
+
+    # 차선 이탈 경고 변경에 대한 콜백 메서드
+    async def on_lane_change(self, data: DataPointReply):
+        lane = data.get(self.Vehicle.ADAS.LaneDepartureDetection.IsWarning).value
+        await self.publish_event(
+            DATABROKER_LANE_SUBSCRIPTION_TOPIC,
+            json.dumps({"laneWarning": lane}),
         )
 
     @subscribe_topic(GET_SPEED_REQUEST_TOPIC)
@@ -108,6 +157,65 @@ class SampleApp(VehicleApp):
                         "status": 0,
                         "message": f"""Current Speed = {vehicle_speed}""",
                     },
+                }
+            ),
+        )
+
+    @subscribe_topic(GET_STEER_REQUEST_TOPIC)
+    async def on_get_steer_request_received(self, data: str) -> None:
+        logger.debug("Received steer request: %s", data)
+        steer = (await self.Vehicle.Chassis.SteeringWheel.Angle.get()).value
+        await self.publish_event(
+            GET_STEER_RESPONSE_TOPIC,
+            json.dumps(
+                {
+                    "result": {
+                        "status": 0,
+                        "message": f"""Steer Angle = {steer}""",
+                    },
+                }
+            ),
+        )
+
+    @subscribe_topic(GET_THROT_REQUEST_TOPIC)
+    async def on_get_throttle_request_received(self, data: str) -> None:
+        logger.debug("Received throttle request: %s", data)
+        throttle = (await self.Vehicle.OBD.ThrottlePosition.get()).value
+        await self.publish_event(
+            GET_THROT_RESPONSE_TOPIC,
+            json.dumps(
+                {
+                    "result": {"status": 0, "message": f"""Throttle = {throttle}"""},
+                }
+            ),
+        )
+
+    @subscribe_topic(GET_BRAKE_REQUEST_TOPIC)
+    async def on_get_brake_request_received(self, data: str) -> None:
+        logger.debug("Received brake request: %s", data)
+        brake = (await self.Vehicle.Chassis.Brake.PedalPosition.get()).value
+        await self.publish_event(
+            GET_BRAKE_RESPONSE_TOPIC,
+            json.dumps(
+                {
+                    "result": {
+                        "status": 0,
+                        "message": f"""Brake\
+                Position = {brake}""",
+                    },
+                }
+            ),
+        )
+
+    @subscribe_topic(GET_LANE_REQUEST_TOPIC)
+    async def on_get_lane_request_received(self, data: str) -> None:
+        logger.debug("Received lane request: %s", data)
+        lane = (await self.Vehicle.ADAS.LaneDepartureDetection.IsWarning.get()).value
+        await self.publish_event(
+            GET_LANE_RESPONSE_TOPIC,
+            json.dumps(
+                {
+                    "result": {"status": 0, "message": f"""Lane Warning = {lane}"""},
                 }
             ),
         )
